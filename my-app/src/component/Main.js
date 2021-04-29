d674ca566b880fe57198c0ec7d0b20b941bf2454
kido <kdio@kidoui-MacBookPro.local>
2021-04-29 15:00:14 +0900

    test commit by kido

---
+++ my-app/src/component/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default function main() {
+
+    return (
+        <div>
+        <div>top contributor</div>    
+        <div>popular</div>
+        <div>lastest</div>
+        </div>
+    )
+}
---
+++ my-app/src/component/recipe/viewrecipe.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+
+
+
+
+
+
+
+export default function viewRecipe () {
+    return (
+        <div>
+            <image></image>
+            <span>제목</span>
+            <button>좋아요</button>
+            <span></span>
+            
+        </div>
+    )
+
+}
