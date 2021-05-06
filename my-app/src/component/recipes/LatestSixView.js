f6913cb5c0e082603dc2bf88a97eb5d3f132b335
Lucky-kor <75019459+Lucky-kor@users.noreply.github.com>
2021-05-06 18:32:43 +0900

    modify : Fix err

--- my-app/src/component/recipes/LatestSixView.js
+++ my-app/src/component/recipes/LatestSixView.js
@@ -1,28 +1,32 @@
 import imgg from "../../img/itemimg.png";
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import {viewRecipe} from '../../actions/index'
+import axios from "axios";
 
-function LatestSixView(props) {
-  return (
-    <div className="LatestSixContainer">
-      <div className="LatestSixHeader">
-        <span className="LatestSixHeaderTitle">Latest</span>
-      </div>
-      <div className="LatestSixAllPrint">
-        {/* 반복문으로 스테이트에 있는 최근의 게시물 6개를 출력합니다. */}
-        {props.list.map((data, idx) => (
-          // <Link to={{ pathname: 'recipe/recipecomment', state:{value: data}}} onClick={handelselectedRecipe} style={{ color: 'inherit', textDecoration: 'none' }}>
-          <div key={idx} onClick={() => props.function(data.title)}>
-            <div className="LatestOneViewArea">
+function LatestSixView (props) {
+
+    return (
+      
+      <div className="LatestSixContainer">
+        <div className="LatestSixHeader">
+          <span className="LatestSixHeaderTitle">Latest</span>
+        </div>
+        <div className="LatestSixAllPrint">
+          {/* 반복문으로 스테이트에 있는 최근의 게시물 6개를 출력합니다. */}
+          {props.list.map((data, idx) => (
+            // <Link to={{ pathname: 'recipe/recipecomment', state:{value: data}}} onClick={handelselectedRecipe} style={{ color: 'inherit', textDecoration: 'none' }}>
+            <div key={idx} onClick={()=>props.function(data)}>
+            <div className="LatestOneViewArea" >
               <div className="LatestOneView">
                 <img alt="" src={imgg} className="LatestOneViewImage" />
                 <div className="LatestOneViewEmptyArea" />
                 <div className="LatestOneViewTagTime">
-                  <span className="LatestOneViewTag">재료</span>
+                  <span className="LatestOneViewTag" >재료</span>
                   <div className="latelyspace" />
                   <div className="latelyLine" />
                   <div className="latelyspace" />
-                  <time datetime="YYYY-MM-DD" className="LatestOneViewTime">
-                    {data.createdAt}
-                  </time>
+                  <time datetime="YYYY-MM-DD"className="LatestOneViewTime" >{data.createdAt}</time>
                 </div>
                 <div className="LatestOneViewEmptyArea" />
                 <div className="LatestOneViewContestView">
@@ -30,11 +34,11 @@ function LatestSixView(props) {
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
 export default LatestSixView;
--- my-app/src/component/recipes/RecipeComment.js
+++ my-app/src/component/recipes/RecipeComment.js
@@ -1,145 +1,159 @@
+// import { Component } from "react";
 import mainimage from "../../img/recipemainimage.png";
 import heartimage from "../../img/heart.png";
 import userimg from "../../img/user.png";
 import bookmark from "../../img/bookmark.png";
+import { logout } from "../../actions";
+// import { selectRecipe } from '../../actions/index';
+// import { useSelector, useDispatch } from 'react-redux';
 
-function RecipeComment({ value }) {
-  const tempdata = {
-    tag: "FRUIT",
-    time: "5 min",
-    title: `${value}`,
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    username: "Andrea Wise",
-    comments: [
-      {
-        username: "KimLucky",
-        time: "5min",
-        comment:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      },
-      {
-        username: "KimLucky",
-        time: "5min",
-        comment:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      },
-      {
-        username: "KimLucky",
-        time: "5min",
-        comment:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      },
-      {
-        username: "KimLucky",
-        time: "5min",
-        comment:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      },
-    ],
-  };
+function RecipeComment (value) {
 
-  return (
-    <div className="RecipeCommentContainer">
-      {/* 레피시 내용 출력 */}
-      <div className="RecipeCommentContent">
-        <img className="RecipeMainImage" src={mainimage} alt="" />{" "}
-        {/* 상단 이미지 출력 */}
-        <div className="RecipeMainUpEmptyArea" />
-        <div className="RecipeMainTagArea">
-          {" "}
-          {/* 태그와 좋아요 버튼 출력하는 구간 */}
-          <div className="RecipeTagEmptyArea" />
-          <div className="RecipeTagContentArea">
-            <span className="RecipeMainTag">{tempdata.tag}</span>
-            <img alt="" className="RecipeHeartImage" src={heartimage} />
-          </div>
-          <div className="RecipeTagEmptyArea" />
-        </div>
-        <div className="RecipeMainUpEmptyArea" />
-        {/* 레시피의 타이틀과 내용을 출력하는 창 */}
-        <div className="RecipeTitleContentArea">
-          <div className="RecipeTitleContentEmptyArea" />
-          <div className="RecipeTitleContentShowArea">
-            <div className="RecipeTitleArea">
-              <span className="RecipeTitle">{tempdata.title}</span>
+  console.log(value)
+
+    const tempdata = {
+      tag: "FRUIT",
+      time: "5 min",
+      title: `${value.value.title}`,
+      content:
+        `${value.value.content}`,
+      username: "Andrea Wise",
+      comments: [
+        {
+          username: "KimLucky",
+          time: "5min",
+          comment:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        },
+        {
+          username: "KimLucky",
+          time: "5min",
+          comment:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        },
+        {
+          username: "KimLucky",
+          time: "5min",
+          comment:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        },
+        {
+          username: "KimLucky",
+          time: "5min",
+          comment:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        },
+      ],
+    }
+
+
+
+
+    return (
+      <div className="RecipeCommentContainer">
+        {/* 레피시 내용 출력 */}
+        <div className="RecipeCommentContent">
+          <img className="RecipeMainImage" src={mainimage} alt="" />{" "}
+          {/* 상단 이미지 출력 */}
+          <div className="RecipeMainUpEmptyArea" />
+          <div className="RecipeMainTagArea">
+            {" "}
+            {/* 태그와 좋아요 버튼 출력하는 구간 */}
+            <div className="RecipeTagEmptyArea" />
+            <div className="RecipeTagContentArea">
+              <span className="RecipeMainTag">{tempdata.tag}</span>
+              <img alt="" className="RecipeHeartImage" src={heartimage} />
             </div>
-            <div className="RecipeContentArea">
-              <span className="RecipeContent">{tempdata.content}</span>
-            </div>
+            <div className="RecipeTagEmptyArea" />
           </div>
-          <div className="RecipeTitleContentEmptyArea" />
-        </div>
-        {/* 하단 작성자의 정보 표시 && 구독 이미지 */}
-        <div className="RecipeUserInfo">
-          <div className="RecipeUserInfoEmptyArea" />
-          <div className="RecipeUserInfoArea">
-            <div className="RecipeUserArea">
-              {" "}
-              {/* 유저 사진과 이름 */}
-              <img alt="" className="RecipeUserImage" src={userimg} />
-              <span className="RecipeCreateUsername">{tempdata.username}</span>
-            </div>
-            <div className="RecipeUserHeartArea">
-              {" "}
-              {/* 시간과 구독버튼 */}
-              <span className="RecipeCreateTime">{tempdata.time}</span>
-              <img alt="" className="RecipeBookmartImage" src={bookmark} />
-            </div>
-          </div>
-          <div className="RecipeUserInfoEmptyArea" />
-        </div>
-      </div>
-      {/* 레피시 내용 출력끝 */}
-      <div className="RecipeCommentEmptyArea" /> {/* 공백 */}
-      {/* 코멘트 출력 */}
-      <div className="RecipeCommentContent">
-        {/* 코멘트 입력창 */}
-        <div className="RecipeCommentInputArea">
-          <div className="RecipeCommentArea">
-            <input
-              type="text"
-              className="CommentInputArea"
-              placeholder="댓글 내용을 작성해주세요"
-            />
-          </div>
-          <div className="RecipeCommentButtonArea">
-            <button className="InputCommentButton">작성</button>
-          </div>
-        </div>
-        {/* 해당 게시물의 모든 코멘트 출력 */}
-        <div className="CurRecipeCommentArea">
-          {tempdata.comments.map((data, idx) => (
-            <div key={idx}>
-              <div className="CurRecipeCommentUpEmptyArea" />
-              <div className="CurRecipeCommentDownArea">
-                <div className="CurRecipeCommentSideEmptyArea" />
-                <div className="CurRecipeCommentPrintArea">
-                  <div className="CurRecipeComment">
-                    <span className="CurRecipeCommentText">{data.comment}</span>{" "}
-                    {/* 코멘트 출력창 */}
-                  </div>
-                  <div className="CurRecipeCommentUserArea">
-                    {" "}
-                    {/* 현재 코멘트 작성자 */}
-                    <span className="CurRecipeCommentUsername">
-                      {data.username}
-                    </span>
-                    <div className="latelyspace" />
-                    <div className="latelyLine" />
-                    <div className="latelyspace" />
-                    <span className="CurRecipeCommentTime">{data.time}</span>
-                  </div>
-                </div>
-                <div className="CurRecipeCommentSideEmptyArea" />
+          <div className="RecipeMainUpEmptyArea" />
+          {/* 레시피의 타이틀과 내용을 출력하는 창 */}
+          <div className="RecipeTitleContentArea">
+            <div className="RecipeTitleContentEmptyArea" />
+            <div className="RecipeTitleContentShowArea">
+              <div className="RecipeTitleArea">
+                <span className="RecipeTitle" >{tempdata.title}</span>
+              </div>
+              <div className="RecipeContentArea">
+                <span className="RecipeContent">{tempdata.content}</span>
               </div>
             </div>
-          ))}
+            <div className="RecipeTitleContentEmptyArea" />
+          </div>
+          {/* 하단 작성자의 정보 표시 && 구독 이미지 */}
+          <div className="RecipeUserInfo">
+            <div className="RecipeUserInfoEmptyArea" />
+            <div className="RecipeUserInfoArea">
+              <div className="RecipeUserArea">
+                {" "}
+                {/* 유저 사진과 이름 */}
+                <img alt="" className="RecipeUserImage" src={userimg} />
+                <span className="RecipeCreateUsername">
+                  {tempdata.username}
+                </span>
+              </div>
+              <div className="RecipeUserHeartArea">
+                {" "}
+                {/* 시간과 구독버튼 */}
+                <span className="RecipeCreateTime">{tempdata.time}</span>
+                <img alt="" className="RecipeBookmartImage" src={bookmark} />
+              </div>
+            </div>
+            <div className="RecipeUserInfoEmptyArea" />
+          </div>
         </div>
+        {/* 레피시 내용 출력끝 */}
+        <div className="RecipeCommentEmptyArea" /> {/* 공백 */}
+        {/* 코멘트 출력 */}
+        <div className="RecipeCommentContent">
+          {/* 코멘트 입력창 */}
+          <div className="RecipeCommentInputArea">
+            <div className="RecipeCommentArea">
+              <input
+                type="text"
+                className="CommentInputArea"
+                placeholder="댓글 내용을 작성해주세요"
+              />
+            </div>
+            <div className="RecipeCommentButtonArea">
+              <button className="InputCommentButton">작성</button>
+            </div>
+          </div>
+          {/* 해당 게시물의 모든 코멘트 출력 */}
+          <div className="CurRecipeCommentArea">
+            {tempdata.comments.map((data, idx) => (
+              <div key={idx}>
+                <div className="CurRecipeCommentUpEmptyArea" />
+                <div className="CurRecipeCommentDownArea">
+                  <div className="CurRecipeCommentSideEmptyArea" />
+                  <div className="CurRecipeCommentPrintArea">
+                    <div className="CurRecipeComment">
+                      <span className="CurRecipeCommentText">
+                        {data.comment}
+                      </span>{" "}
+                      {/* 코멘트 출력창 */}
+                    </div>
+                    <div className="CurRecipeCommentUserArea">
+                      {" "}
+                      {/* 현재 코멘트 작성자 */}
+                      <span className="CurRecipeCommentUsername">
+                        {data.username}
+                      </span>
+                      <div className="latelyspace" />
+                      <div className="latelyLine" />
+                      <div className="latelyspace" />
+                      <span className="CurRecipeCommentTime">{data.time}</span>
+                    </div>
+                  </div>
+                  <div className="CurRecipeCommentSideEmptyArea" />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* 코멘트 출력끝 */}
       </div>
-      {/* 코멘트 출력끝 */}
-    </div>
-  );
-}
+    );
+  }
 
 export default RecipeComment;
